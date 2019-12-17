3d03c2b68aca612ed85b80765cb4fc21b0d44e41
Chinmay Garde <chinmaygarde@google.com>
2019-12-16 16:10:12 -0800

    Work on wiring up Vulkan rendering.

---
+++ source/core/logging.h
@@ -0,0 +1,39 @@
+// This source file is part of doxygen2docset.
+// Licensed under the MIT License. See LICENSE.md file for details.
+
+#pragma once
+
+#include <iostream>
+
+#include "macros.h"
+
+namespace pixel {
+
+template <class Type>
+class AutoLogger {
+ public:
+  AutoLogger(Type& logger) : logger_(logger) {}
+
+  ~AutoLogger() {
+    logger_ << std::endl;
+    logger_.flush();
+  }
+
+  template <class T>
+  AutoLogger& operator<<(const T& object) {
+    logger_ << object;
+    return *this;
+  }
+
+ private:
+  Type& logger_;
+
+  P_DISALLOW_COPY_AND_ASSIGN(AutoLogger);
+};
+
+using AutoOStreamLogger = AutoLogger<std::ostream>;
+
+#define P_ERROR ::pixel::AutoOStreamLogger(std::cerr)
+#define P_LOG ::pixel::AutoOStreamLogger(std::cout)
+
+}  // namespace pixel
--- source/core/macros.h
+++ source/core/macros.h
@@ -1,4 +1,18 @@
 
+#pragma once
+
+#include <cassert>
+
 #define P_DISALLOW_COPY_AND_ASSIGN(TypeName) \
   TypeName(const TypeName&) = delete;        \
   TypeName& operator=(const TypeName&) = delete;
+
+#if defined(__GNUC__) || defined(__clang__)
+#define P_WARN_UNUSED_RESULT __attribute__((warn_unused_result))
+#else
+#define P_WARN_UNUSED_RESULT
+#endif
+
+#define P_ASSERT(x) assert((x))
+
+#define P_ALLOW_UNUSED_LOCAL(x) false ? (void)x : (void)0
