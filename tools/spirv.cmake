8c9e6257cd2b535e5cefd3cba93e7ebca8c266fb
Chinmay Garde <chinmaygarde@google.com>
2019-12-22 18:00:15 -0800

    Add build rules for shader compilation.

---
+++ tools/spirv.cmake
@@ -0,0 +1,35 @@
+if(__spirv)
+  return()
+endif()
+set(__spirv INCLUDED)
+
+function(compile_shader TARGET SHADER_PATH)
+
+  find_program(GLSLC_PROG glslc)
+
+  get_filename_component(SHADER_NAME ${SHADER_PATH} NAME)
+
+  get_filename_component(SHADER_IN_PATH ${SHADER_PATH} ABSOLUTE)
+  set(SHADER_OUT_PATH ${CMAKE_BINARY_DIR}/shaders/${SHADER_NAME}.spv)
+  get_filename_component(SHADER_OUT_DIR ${SHADER_OUT_PATH} DIRECTORY)
+
+  file(MAKE_DIRECTORY ${SHADER_OUT_DIR})
+
+  add_custom_command(
+    OUTPUT ${SHADER_OUT_PATH}
+    COMMAND ${GLSLC_PROG} ${SHADER_IN_PATH} -o ${SHADER_OUT_PATH}
+    DEPENDS ${SHADER_IN_PATH}
+    IMPLICIT_DEPENDS CXX ${SHADER_IN_PATH}
+  )
+
+  set_source_files_properties(${SHADER_OUT_PATH} PROPERTIES GENERATED TRUE)
+  target_sources(${TARGET} PRIVATE ${SHADER_OUT_PATH})
+
+endfunction()
+
+
+function(compile_shaders TARGET)
+  foreach(shader ${ARGN})
+    compile_shader(${TARGET} ${shader})
+  endforeach()
+endfunction()
