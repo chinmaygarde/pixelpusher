d2e721477359deedc1e3ee2bffeb2f83f0f9b428
Chinmay Garde <chinmaygarde@gmail.com>
2020-05-04 21:48:54 -0700

    Fix sublime solution file for inline error.s

--- source/machine/vulkan.h
+++ source/machine/vulkan.h
@@ -2,12 +2,12 @@
 
 #define VK_NO_PROTOTYPES
 
+#define VULKAN_HPP_NO_EXCEPTIONS
+#include <vulkan/vulkan.hpp>
+
 #define GLFW_INCLUDE_NONE
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-
-#define VULKAN_HPP_NO_EXCEPTIONS
-#include <vulkan/vulkan.hpp>
 
 namespace pixel {
 
